5d79ed5ef8a72a3ccfb9fbc6df47bc71ffa03a08
umbertodomenicociccia <umbertociccia@icloud.com>
2024-04-16 15:44:59 +0200

    added spring security

--- src/main/java/com/unicalsocial/backend/configurations/SecurityConfig.java
+++ src/main/java/com/unicalsocial/backend/configurations/SecurityConfig.java
@@ -1,18 +1,28 @@
 package com.unicalsocial.backend.configurations;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthConverter jwtAuthConverter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf((csrf) -> csrf
-                .ignoringRequestMatchers("/api/**"));
+        httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        httpSecurity.oauth2ResourceServer((oauth2) -> oauth2.jwt((jwt) -> jwt.jwtAuthenticationConverter(jwtAuthConverter)));
+        httpSecurity.sessionManagement((session) -> session.sessionCreationPolicy(STATELESS));
         return httpSecurity.build();
     }
 }
