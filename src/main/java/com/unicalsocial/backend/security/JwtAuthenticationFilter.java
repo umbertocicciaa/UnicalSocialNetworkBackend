2b9824fe56f61f075c862c2434cfd62fb099a56f
umbertodomenicociccia <umbertociccia@icloud.com>
2024-05-18 11:01:08 +0200

    refactoring

--- src/main/java/com/unicalsocial/backend/security/JwtAuthenticationFilter.java
+++ src/main/java/com/unicalsocial/backend/security/JwtAuthenticationFilter.java
@@ -23,7 +23,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-  private final JwtService jwtService;
+  private final JwtService jwtServiceImpl;
   private final UserDetailsService userDetailsService;
   private final TokenRepository tokenRepository;
 
@@ -45,13 +45,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
       return;
     }
     jwt = authHeader.substring(7);
-    userEmail = jwtService.extractUsername(jwt);
+    userEmail = jwtServiceImpl.extractUsername(jwt);
     if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
       UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
       var isTokenValid = tokenRepository.findByToken(jwt)
           .map(t -> !t.isExpired() && !t.isRevoked())
           .orElse(false);
-      if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
+      if (jwtServiceImpl.isTokenValid(jwt, userDetails) && isTokenValid) {
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
             userDetails,
             null,
