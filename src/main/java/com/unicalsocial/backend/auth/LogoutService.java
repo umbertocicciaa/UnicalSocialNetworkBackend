5084806a0a7735873a6255c7625abc8596fa3d3b
umbertodomenicociccia <umbertociccia@icloud.com>
2024-05-20 16:34:29 +0200

    logout

---
+++ src/main/java/com/unicalsocial/backend/auth/LogoutService.java
@@ -0,0 +1,38 @@
+package com.unicalsocial.backend.auth;
+
+import com.unicalsocial.backend.token.TokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+
+    private final TokenRepository tokenRepository;
+
+    @Override
+    public void logout(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        jwt = authHeader.substring(7);
+        var storedToken = tokenRepository.findByToken(jwt);
+        if (storedToken.isPresent()) {
+            storedToken.get().setExpired(true);
+            storedToken.get().setRevoked(true);
+            tokenRepository.save(storedToken.get());
+            SecurityContextHolder.clearContext();
+        }
+    }
+}
